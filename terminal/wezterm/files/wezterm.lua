545657b179410cf56c7742d9c6348b138fab7c76
Shawn Wang <shawn111@gmail.com>
2024-03-02 20:39:24 +0800

    change wezterm to UltraDark via comtrya

--- terminal/wezterm/files/wezterm.lua
+++ terminal/wezterm/files/wezterm.lua
@@ -7,7 +7,9 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
+-- config.color_scheme = 'AdventureTime'
+config.color_scheme = 'UltraDark'
+
 
 -- and finally, return the configuration to wezterm
 return config
