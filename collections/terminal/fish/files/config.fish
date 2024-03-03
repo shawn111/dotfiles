3b862088780ee5200527530994914c1e5e9fecc9
Shawn Wang <shawn111@gmail.com>
2024-03-03 09:23:19 +0800

    add zellij alias

--- collections/terminal/fish/files/config.fish
+++ collections/terminal/fish/files/config.fish
@@ -1,3 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+export EDITOR=hx
+alias zr="zellij run"
+alias ze="zellij edit"
+alias zrf="zellij run --floating"
+alias zef="zellij edit --floating"
