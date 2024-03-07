4d4e7d6d59849d2a613f155b7f716821f2e5b911
Shawn Wang <shawn111@gmail.com>
2024-03-07 09:57:18 +0800

    support atuin / zoxide in fish

--- collections/terminal/fish/files/config.fish
+++ collections/terminal/fish/files/config.fish
@@ -1,9 +1,18 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    atuin init fish --disable-up-arrow | source
+    zoxide init fish | source
 end
 
 export EDITOR=hx
-alias zr="zellij run"
+#zellij setup --generate-completion fish
+alias zr="zellij run --"
 alias ze="zellij edit"
-alias zrf="zellij run --floating"
+alias zrf="zellij run --floating --"
 alias zef="zellij edit --floating"
+
+if test -z (pgrep ssh-agent | string collect)
+    eval (ssh-agent -c)
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+end
