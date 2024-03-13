94a4840b07a8e3c03fb7e272615775939270bd6a
Shawn Wang <shawn111@gmail.com>
2024-03-13 14:08:02 +0800

    kubeconfig

---
+++ collections/terminal/fish/files/conf.d/kubesess.fish
@@ -0,0 +1,4 @@
+export KUBECONFIG=$HOME/.kube/config:$HOME/.kube/19k3s.config:$HOME/.kube/kind.config
+#export KUBECONFIG=$HOME/.kube/19k3s.config:$HOME/.kube/kind.config
+#export KUBECONFIG=$SESSION_CONTEXT:$KUBECONFIG
+
